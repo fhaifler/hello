f3d79f3f8f1d9d16a6facf99d090200604622d67
Frantisek Haifler <frantisek.haifler@trackdesk.com>
2022-06-11 13:33:04 +0200

    feat(cmd/root): Add --name parameter

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -3,16 +3,28 @@ package root
 import (
 	"fmt"
 
-	"github.com/spf13/cobra"
-
 	"hello/version"
+
+	"github.com/spf13/cobra"
 )
+
+var FlagName string
 
 var Cmd = &cobra.Command{
 	Use:   "hello",
 	Short: "hello, " + version.Version,
 	Args:  cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
-		fmt.Println("Hello World!")
+		fmt.Printf("Hello %s!\n", FlagName)
 	},
+}
+
+func init() {
+	Cmd.Flags().StringVarP(
+		&FlagName,
+		"name",
+		"n",
+		"World",
+		"Name to say hello",
+	)
 }
